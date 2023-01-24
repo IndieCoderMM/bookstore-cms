4356c8acb6cdcd18bbec804ea5f86c05e0f05259
hthantoo <hthant00chk@gmail.com>
2023-01-24 15:19:41 +0630

    Implement remove-book feature

---
+++ src/components/ButtonGroup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/books';
+
+const ButtonGroup = ({ id }) => {
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(removeBook({ id }));
+  };
+  return (
+    <div className="btn-group">
+      <button type="button">Comment</button>
+      <div className="vertical-divider" />
+      <button type="button" onClick={handleRemove}>
+        Remove
+      </button>
+      <div className="vertical-divider" />
+      <button type="button">Edit</button>
+    </div>
+  );
+};
+
+ButtonGroup.propTypes = {
+  id: PropTypes.string.isRequired,
+};
+
+export default ButtonGroup;
